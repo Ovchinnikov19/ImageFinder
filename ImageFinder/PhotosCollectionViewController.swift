97f5f22228330a50b07103c3a03289642bb8da25
Ovchinnikov19 <r1dd1k1919@gmail.com>
2019-09-24 03:59:37 +0300

    Setup PhotosCollection: header, cellForItemAt, numberOfItemsInSection

--- ImageFinder/PhotosCollectionViewController.swift
+++ ImageFinder/PhotosCollectionViewController.swift
@@ -15,6 +15,31 @@ class PhotosCollectionViewController: UICollectionViewController {
         collectionView.backgroundColor = .orange
         
         print("PhotoCollection")
+        
+        setupCollectionView()
+        setupNavigationBar()
     }
     
+    private func setupCollectionView(){
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellId")
+    }
+    
+    private func setupNavigationBar() {
+        let titleLabel = UILabel()
+        titleLabel.text = "PHOTOS"
+        titleLabel.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+        titleLabel.textColor = #colorLiteral(red: 0.5019607843, green: 0.4980392157, blue: 0.4980392157, alpha: 1)
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(customView: titleLabel)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CellId", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
 }
