3033695d0768c1ae46f6612883303175692b05e3
Ovchinnikov19 <r1dd1k1919@gmail.com>
2019-09-27 17:09:46 +0300

    Add use dataTask in NetworkService

--- ImageFinder/Service/NetworkService.swift
+++ ImageFinder/Service/NetworkService.swift
@@ -10,13 +10,16 @@ import Foundation
 
 class NetworkService {
     
-    func request(searchTerm: String, completion: (Data?, Error?) -> Void) {
+    func request(searchTerm: String, completion: @escaping (Data?, Error?) -> Void) {
         
         let parameters = prepareParameters(searchTerm: searchTerm)
         let url = createUrl(params: parameters)
         var request = URLRequest(url: url)
         request.allHTTPHeaderFields = prepareHeader()
         request.httpMethod = "GET"
+        
+        let task = createDataTask(from: request, completion: completion)
+        task.resume()
     }
     
     private func prepareHeader() -> [String: String]? {
@@ -43,4 +46,13 @@ class NetworkService {
         components.queryItems = params.map { URLQueryItem(name: $0, value: $1)}
         return components.url!
     }
+    
+    private func createDataTask(from request: URLRequest, completion: @escaping (Data?, Error?) -> Void) -> URLSessionDataTask {
+        return URLSession.shared.dataTask(with: request) { (data, response, error) in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+        }
+    }
 }
+ 
