9ee1d5f796db4a7f740a9a2ddb6c9f19ef879e91
Ovchinnikov19 <r1dd1k1919@gmail.com>
2019-09-28 03:36:11 +0300

    Add PhotoCell for PhotosCollectionVC with setup photoImageView and checkMark

---
+++ ImageFinder/PhotoCell.swift
@@ -0,0 +1,55 @@
+//
+//  PhotoCell.swift
+//  ImageFinder
+//
+//  Created by Eduard Ovchinnikov on 28/09/2019.
+//  Copyright © 2019 Eduard Ovchinnikov. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    static let reuseId = "PhotoCell"
+    
+    private let checkMark: UIImageView = {
+        let image = UIImage(named: "checkmark")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.alpha = 0
+        return imageView
+    }()
+    
+    private let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupPhotoImageView()
+        setupCheckMarkView() 
+    }
+    
+    private func setupPhotoImageView() {
+        addSubview(photoImageView)
+        photoImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        photoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        photoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        photoImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+    }
+    
+    private func setupCheckMarkView() {
+        addSubview(checkMark)
+        checkMark.trailingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: -8).isActive = true
+        checkMark.bottomAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: -8).isActive = true
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
