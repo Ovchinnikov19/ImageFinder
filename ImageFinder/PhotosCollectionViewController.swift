99f8976c35784fc433befbb97929ef21593f2a18
Ovchinnikov19 <r1dd1k1919@gmail.com>
2019-09-29 15:20:34 +0300

    add extension UICollectionViewDelegateFlowLayout with divide on 2 column

--- ImageFinder/PhotosCollectionViewController.swift
+++ ImageFinder/PhotosCollectionViewController.swift
@@ -14,6 +14,9 @@ class PhotosCollectionViewController: UICollectionViewController {
     private var timer: Timer?
     private var photos = [UnsplashPhoto]()
     
+    private let itemsPerRow:CGFloat = 2
+    private let sectionInserts = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+    
     private lazy var addBarButtonItem: UIBarButtonItem = {
         return UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBarButtonTapped))
     }()
@@ -24,7 +27,7 @@ class PhotosCollectionViewController: UICollectionViewController {
     
     override func viewDidLoad() {
         
-        collectionView.backgroundColor = .orange
+        collectionView.backgroundColor = .white
         
         setupCollectionView()
         setupNavigationBar()
@@ -44,6 +47,9 @@ class PhotosCollectionViewController: UICollectionViewController {
     private func setupCollectionView(){
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CellId")
         collectionView.register(PhotoCell.self, forCellWithReuseIdentifier: PhotoCell.reuseId)
+        
+        collectionView.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        collectionView.contentInsetAdjustmentBehavior = .automatic
     }
     
     private func setupNavigationBar() {
@@ -92,4 +98,26 @@ extension PhotosCollectionViewController: UISearchBarDelegate {
             }
         })
     }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension PhotosCollectionViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let photo = photos[indexPath.item]
+        let paddingSpace = sectionInserts.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        let height = CGFloat(photo.height) * widthPerItem / CGFloat(photo.width)
+        return CGSize(width: widthPerItem, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+         return sectionInserts
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInserts.left
+    }
+    
 }
